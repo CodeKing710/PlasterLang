835aa14c35b6ae927a4cbfeb928e465e6714d72f
SubLock69 <cjrox01@yahoo.com>
2018-04-17 17:35:42 -0500

    Create compiler.rb
    All code is theoretical, hence the comments

---
+++ Ruby implementation/compiler.rb
@@ -0,0 +1,6 @@
+#Lexer
+#take input, lexical analysis, output tree
+#Parser
+#Run through tree, output intermediate code (translated to runnable ruby)
+#Execution
+#Take the new code and run a ruby instance with it
